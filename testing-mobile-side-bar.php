d29af54b7d85ba478c8615e760b005aa010888fd
ms14331 <suarezandrew42@gmail.com>
2018-01-31 10:51:54 +0800

    adding navbar left  31/01/2018

---
+++ testing-mobile-side-bar.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+*Template: testing mobile sidebar
+*/
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<title><?php wp_title(''); ?></title>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo ( 'template_url' ); ?>/css/custom-michael.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo ( 'template_url' ); ?>/css/custom-list.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     <script src="<?php echo JS; ?>/jquery.min.js"></script>
+    <script src="<?php echo JS; ?>/respond.min.js"></script>
+    <script src="<?php echo JS; ?>/custom-michael.js"></script>
+    <script src="<?php echo JS; ?>/bootstrap.min.js"></script>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+	
+</body>
+</html>
